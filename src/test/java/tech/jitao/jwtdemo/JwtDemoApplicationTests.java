53f081f9f8772328408706fda054ceaa5fb8031f
jitao <jitao@jitao.tech>
2021-02-23 11:58:38 +0800

    init

---
+++ src/test/java/tech/jitao/jwtdemo/JwtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.jitao.jwtdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
